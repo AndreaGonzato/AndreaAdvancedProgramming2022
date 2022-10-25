57ef7f14671907551e0ecc09356edb70c20bc491
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-10-25 10:14:11 +0200

    WIP

--- homeworks/lecture04/es2/include/complex.hpp
+++ homeworks/lecture04/es2/include/complex.hpp
@@ -4,44 +4,66 @@
 #include <iostream>
 #include <typeinfo>
 
-template <typename T>
+
+template <typename R, typename I>
 class Complex{
     public:
-        T real = 0;
-        T imaginary = 0;
+        R real = 0;
+        I imaginary = 0;
 
         Complex(){
             real = 0;
             imaginary = 0;
         };
-        Complex(T r, T i){
+        Complex(R r, I i){
             real = r;
             imaginary = i;
         };
+
+        void printType(){
+            if constexpr(std::is_same_v<int,R>){
+                std::cout<<"this complex number store the real as an int"<<std::endl;
+            }
+            if constexpr(std::is_same_v<int, I>){
+                std::cout<<"this complex number store the imaginary part as an int"<<std::endl;
+            }
+            if constexpr(std::is_same_v<double, R>){
+                std::cout<<"this complex number store the real part as a double"<<std::endl;
+            }
+            if constexpr(std::is_same_v<double, I>){
+                std::cout<<"this complex number store the imaginary part as a double"<<std::endl;
+            }
+        };
+
         void print(){
-            if constexpr(std::is_same_v<int,T>){
-                std::cout<<"this complex number store real and imaginary as int"<<std::endl;
-            }
-            if constexpr(std::is_same_v<double,T>){
-                std::cout<<"this complex number store real and imaginary as double"<<std::endl;
-            }
+            printType();
             std::cout<<real << " i "<< imaginary << std::endl;
         };
 
-        template <typename M>
-        Complex operator+(const Complex<M>& b) {
+        template <typename M, typename N>
+        Complex operator+(const Complex<M, N>& b) {
+            if constexpr(std::is_same_v<double, M>){
+                std::cout<<"we are summing a real part that is double"<<std::endl;
+            }
+            if constexpr(std::is_same_v<double, N>){
+                std::cout<<"we are summing a imaginary part that is double"<<std::endl;
+            }
+                        
+
             Complex result;
             result.real = real + b.real;
             result.imaginary = imaginary + b.imaginary;
+            result.printType();
             return result;
         }
 
 
-        template <typename M>
-        Complex operator*(const Complex<M>& b) {
+        template <typename M, typename N>
+        Complex operator*(const Complex<M, N>& b) {
             Complex result;
             result.real = real * b.real - imaginary * b.imaginary;
             result.imaginary = real * b.imaginary + imaginary * b.real;
+            result.printType();
             return result;
         }
 
--- homeworks/lecture04/es2/main.cpp
+++ homeworks/lecture04/es2/main.cpp
@@ -2,14 +2,13 @@
 #include "complex.hpp"
 
 int main(){
-    Complex<int> c1 = Complex(2, 1);
-    Complex<double> c2 = Complex(6.9, 8.9);
+    Complex<int, int> c1 = Complex(2, 1);
+    Complex<double, double> c2 = Complex(6.9, 8.9);
 
     c1.print();
-    c2.print();
 
     std::cout<<"the sum result is:"<<std::endl;
-    Complex c3 = c2+c1;
+    Complex c3 = c1+c2;
     c3<<std::cout;
 
 
